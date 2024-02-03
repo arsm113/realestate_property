7f32e5ffc083c22b84e4b2b1b971ec40422008d3
Arnab Samanta <92151127+arsm113@users.noreply.github.com>
2024-02-04 03:46:32 +0530

    Update home.py
    gurgaon

--- home.py
+++ home.py
@@ -4,30 +4,32 @@ def home():
     st.title("Real Estate Data Science Portfolio Project")
 
     st.write(
-        "Welcome to my Real Estate Data Science Portfolio Project! In this project, I have developed an app "
-        "with different segments to demonstrate my skills and projects in the field of real estate data analysis."
+        "Welcome to my Real Estate Data Science Portfolio Project! This project focuses on real estate data analysis "
+        "with a primary focus on properties in Gurgaon. I have developed an app with three main segments to showcase "
+        "different aspects of the real estate market."
     )
 
     st.markdown("### Prediction Page")
     st.write(
-        "Explore the Prediction Page to make predictions on real estate-related data. "
-        "Use machine learning models to forecast prices or other relevant metrics based on the provided input."
+        "Explore the Prediction Page to make predictions related to Gurgaon's real estate market. "
+        "Use machine learning models to forecast property prices or other relevant metrics based on the provided input."
     )
 
     st.markdown("### Analysis Page")
     st.write(
-        "The Analysis Page allows you to dive deep into the real estate dataset. Visualize key trends, "
-        "analyze property features, and gain insights into the dynamics of the real estate market."
+        "The Analysis Page allows you to delve into the Gurgaon real estate dataset. Visualize key trends, analyze property features, "
+        "and gain insights into the dynamics of the real estate market in Gurgaon."
     )
 
     st.markdown("### Recommendation Page")
     st.write(
-        "In the Recommendation Page, discover personalized recommendations for properties based on your preferences "
-        "and historical data. The recommendation algorithms aim to provide tailored suggestions for potential buyers or investors."
+        "In the Recommendation Page, discover personalized recommendations for Gurgaon properties based on preferences and historical data. "
+        "The recommendation algorithms aim to provide tailored suggestions for potential buyers or investors in the Gurgaon market."
     )
 
     st.write(
-        "Feel free to navigate through the different segments using the sidebar navigation. "
+        "This project is currently focused on Gurgaon property, but stay tuned! We have plans to expand our analysis to include more cities "
+        "in the future. Feel free to navigate through the different segments using the sidebar navigation. "
         "Explore the features and functionalities of the Real Estate Data Science Portfolio Project!"
     )
 
